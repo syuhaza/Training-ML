6b01e705f29954781d6bfbc1983edf6428c4d82c
syuhaza <syuhadaza20@gmail.com>
2024-06-28 11:33:54 +0800

    new file:   myclass.py new file:   oop.py

---
+++ myclass.py
@@ -0,0 +1,47 @@
+# principle = 1000
+# period = 1
+# rate = 6
+# simpleInterest = (principle * period * rate) /100
+# print("Simple Interest:", simpleInterest)
+
+"""
+def calculateSimpleInterest(principle, period, rate):
+    simpleInterest = (principle * period * rate) /100
+    return simpleInterest
+
+def calculateTotalAmount(principle, simpleInterest):
+    return principle + simpleInterest
+
+
+principle = 1000
+period = 1
+rate = 6
+result = calculateSimpleInterest(principle, period, rate)
+newResult = calculateTotalAmount(principle,result)
+
+print("Simple Interest:", result)
+print("Total amount to be paid:", newResult)
+
+"""
+
+# 
+class Student:
+
+    # 1) Method is nothing but a function that inside a class
+    # 2) Method atleast 1 parameter
+    # 3) 
+    def __init__(self):
+        print("Object is created")
+
+    # This is our first method/function inside the class student
+    # remember methods must have atleast 1 parameter
+    def attendClass(self):
+        print("Object started attending the class")
+    
+    def doProject(self, projectname):
+        print("Object started doing the project:", projectname)
+
+# Let us create our first object
+zul  = Student()
+zul.attendClass()
+zul.doProject("Pokemon")
